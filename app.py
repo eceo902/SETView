fdd3169411f9f8b674b644f5d821d0d0ce103c94
eceo902 <evan.seeyave@gmail.com>
2020-10-25 16:12:29 -0600

    Bootstrap and templates
    Adding Bootstrap and getting some html files written.

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from key import Key
+from key import my_api_key
 from newsapi import NewsApiClient
 from datetime import timedelta
 
+newsapi = NewsApiClient(api_key=my_api_key)
+
+
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
+@app.route("/top")
+def top():
+    top_news = newsapi.get_top_headlines()
 
+
+@app.route("/all")
+def all():
+    top_news = newsapi.get_everything()
 
 
 
--- key.py
+++ key.py
@@ -1,1 +1,1 @@
-Key = "a14f943cff544e4fa138bcd07daf54bc"
+my_api_key = "a14f943cff544e4fa138bcd07daf54bc"
