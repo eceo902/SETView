a5a442758234a4f837a9c7bb443bcff5eb73acbf
eceo902 <evan.seeyave@gmail.com>
2020-10-25 18:39:49 -0600

    More pages
    Adding more files in redirects

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ def home():
 
 @app.route("/top")
 def top():
-    top_news = newsapi.get_top_headlines(category="technology")
+    top_news = newsapi.get_top_headlines(category="technology, sports, entertainment", country="us", language="en", page_size=50)
+    if(top_news["status"] != "ok"):
+        flash("There was an error!, Try again")
+        return render_template("trending.html")
+    elif(top_news["totalResults"] == 0):
+        flash("There are no articles right now.  Sorry, try again later!")
+        return render_template("trending.html")
+    else:
+        articles = top_news["articles"]
+        return render_template("trending.html", articles=articles)
 
 
 @app.route("/all")
 def all():
     top_news = newsapi.get_everything()
 
+@app.route("/<category>")
+def cat(category):
+    cat_news = newsapi.get_top_headlines(category=category, country="us", language="en", page_size=50)
 
 
 if __name__ == "__main__":
