62884c3fa50aff09cf455b9f4b3de52115cf7ebb
eceo902 <evan.seeyave@gmail.com>
2020-10-25 23:48:03 -0600

    Fixing
    Fixing All News page and adding more pages

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from key import my_api_key
 from newsapi import NewsApiClient
 from datetime import timedelta
+import requests
 
 newsapi = NewsApiClient(api_key=my_api_key)
 
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
+
 
 @app.route("/")
 def home():
@@ -15,7 +18,8 @@ def home():
 
 @app.route("/top")
 def top():
-    top_news = newsapi.get_top_headlines(category="technology, sports, entertainment", country="us", language="en", page_size=50)
+    url = "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&category=sports&category=technology&pageSize=50&apiKey=a14f943cff544e4fa138bcd07daf54bc"
+    top_news = requests.get(url).json()
     if(top_news["status"] != "ok"):
         flash("There was an error!, Try again")
         return render_template("trending.html")
@@ -27,13 +31,49 @@ def top():
         return render_template("trending.html", articles=articles)
 
 
-@app.route("/all")
-def all():
-    top_news = newsapi.get_everything()
+@app.route("/all/<int:page_number>")
+def all_things(page_number=None):
+    all_news = newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports", language="en", page_size=10, page=page_number)
+    if (all_news["status"] != "ok"):
+        flash("There was an error!, Try again")
+        return render_template("everything.html", current_page=1)
+    elif (all_news["totalResults"] == 0):
+        flash("There are no articles there right now.  Sorry, try again later!")
+        return render_template("everything.html", current_page=1)
+    elif(page_number == None):
+        articles = all_news["articles"]
+        return render_template("everything.html", articles=articles, current_page=1)
+    else:
+        articles = all_news["articles"]
+        return render_template("everything.html", articles=articles, current_page=page_number)
+
 
 @app.route("/<category>")
 def cat(category):
-    cat_news = newsapi.get_top_headlines(category=category, country="us", language="en", page_size=50)
+    cat_news = newsapi.get_top_headlines(category=category.lower(), country="us", language="en", page_size=50)
+    if (cat_news["status"] != "ok"):
+        flash("There was an error!, Try again")
+        return render_template("categories.html")
+    elif (cat_news["totalResults"] == 0):
+        flash("There are no articles right now.  Sorry, try again later!")
+        return render_template("categories.html")
+    else:
+        articles = cat_news["articles"]
+        return render_template("categories.html", articles=articles, type=category)
+
+
+
+@app.route("/set", methods=["POST", "GET"])
+def set_preferences():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("")
+
+
+@app.route("/interests")
+def interest():
+    pass
 
 
 if __name__ == "__main__":
